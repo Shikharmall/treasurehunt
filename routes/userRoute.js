0e50738cb05daeacb9d573221f8161ff2271fef5
Shikharmall <107389374+Shikharmall@users.noreply.github.com>
2023-04-17 00:39:53 +0530

    Update userRoute.js

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -19,18 +19,23 @@ user_route.use(express.static('public')); //for static files....
 
 const userController = require('../controllers/userController');
 
-//user_route.get('/',auth.isLogout,userController.loginLoad);
+//login
 user_route.get('/',userController.loginLoad);
 user_route.get('/login',userController.loginLoad);
 
+//verify login
 user_route.post('/login',userController.verifyLogin);
 
+//home page
 user_route.get('/home',userController.loadHome);
+
+//global rank page
 user_route.get('/rankdashboard',userController.loadrankdashboard);
 
-
+//logout
 user_route.get('/logout',userController.userLogout);
 
+//editprofile for score
 user_route.post('/edit',userController.editProfile);
 
 
