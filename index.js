c0737615b5ef3ecc4de543e31321e72b20d03dc2
Shikhar Mall <shmall.21.2020@gmail.com>
2024-01-29 02:04:10 +0530

    added env

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
+require("dotenv").config();
+
 const mongoose = require("mongoose");
-//mongoose.connect("mongodb://127.0.0.1:27017/treasurehunt");
 
-mongoose.connect("mongodb+srv://shmall212020:treasurehuntgame@treasurehuntgame.y0ags4n.mongodb.net/?retryWrites=true&w=majority");
+if (process.env.DATABASE === "MONGODBATLAS") {
+  mongoose.connect(process.env.DATABASEURL);
+  mongoose.connection.on("error", (err) => {
+    console.log("Connection Failed");
+  });
+  mongoose.connection.on("connected", (connected) => {
+    console.log("Connected to MongoDB Atlas.");
+  });
+} else if (process.env.DATABASE === "MONGODBLOCAL") {
+  mongoose.connect("mongodb://127.0.0.1:27017/treasurehunt");
+  mongoose.connection.on("error", (err) => {
+    console.log("Connection Failed");
+  });
+  mongoose.connection.on("connected", (connected) => {
+    console.log("Connected to MongoDB Atlas.");
+  });
+} else {
+  console.log("No proper ENV.");
+}
 
-
-
-const express = require('express');
+const express = require("express");
 const app = express();
 
 //for user route
-const userRoute = require('./routes/userRoute');
+const userRoute = require("./routes/userRoute");
 
-app.use('/', userRoute);
+app.use("/", userRoute);
 
-
-app.listen(3000,function(){
-    console.log("Server is running....");
+app.listen(3000, function () {
+  console.log("Server is running....");
 });
